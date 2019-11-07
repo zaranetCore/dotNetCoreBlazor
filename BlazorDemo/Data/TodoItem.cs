d7a28b4febdaaa3e2ead5e039794a1354483b47d
zaranetCore <zaranet@163.com>
2019-11-07 17:20:02 +0800

    this is a test

---
+++ BlazorDemo/Data/TodoItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorDemo.Data
+{
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+        public string id { get; set; }
+    }
+}
---
+++ BlazorDemo/Data/TodoItemService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorDemo.Data
+{
+    public class TodoItemService
+    {
+        public Task<List<TodoItem>> GetTodoItems()
+        {
+            List<TodoItem> list = new List<TodoItem>();
+            list.Add(new TodoItem() { 
+                IsDone = false, Title = "zaranet",id = Guid.NewGuid().ToString()
+            });
+            return Task.FromResult(list);
+        }
+    }
+}
